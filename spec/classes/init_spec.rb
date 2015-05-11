e0bae8c4870defbc9aeb640b79f899ab41ff1b43
Paul Badcock <paul.badcock@novascotia.ca>
2015-05-11 11:11:32 -0300

    Broken tests but full coverage

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -3,5 +3,96 @@ describe 'splunkuf' do
 
   context 'with defaults for all parameters' do
     it { should contain_class('splunkuf') }
+    it { should contain_class('splunkuf::params') }
+  end
+
+  context 'on RHEL7' do
+    let :facts do
+      {
+        :osfamily                   => 'RedHat',
+        :operatingsystemmajrelease  => '7',
+        :architecure                => 'amd64',
+      }
+    end
+
+    describe 'should install splunk forwarder package' do
+      let(:params) do
+        {
+          :targeturi => 'splunkwoo:8089',
+        }
+      end
+
+      it { should contain_splunkuf__installpkg('splunkforwarder').with_ensure('latest') }
+    end
+
+    it do
+      should contain_file('/usr/lib/systemd/system/splunkforwarder.service').with({
+        'ensure'  => 'file',
+        'owner'   => 'root',
+        'group'   => 'root',
+        'mode'    => '0755',
+        })
+    end
+
+    it do
+      should contain_file('/opt/splunkforwarder/etc/system/local/deploymentclient.conf').with({
+        'ensure'  => 'file',
+        'owner'   => 'root',
+        'group'   => 'root',
+        'mode'    => '0644',
+        })
+    end
+
+    it do
+      should contain_service('splunkforwarder').with({
+        'ensure'     => 'running',
+        'enable'     => 'true',
+      })
+    end
+  end
+
+  context 'on RHEL6' do
+    let :facts do
+      {
+        :osfamily                   => 'RedHat',
+        :operatingsystemmajrelease  => '6',
+        :architecure                => 'amd64',
+      }
+    end
+
+    describe 'should install splunk forwarder package' do
+      let(:params) do
+        {
+          :targeturi => 'splunkwoo:8089',
+        }
+      end
+
+      it { should contain_splunkuf__installpkg('splunkforwarder').with_ensure('latest') }
+    end
+
+    it do
+      should contain_file('/etc/init.d/splunkforwarder').with({
+        'ensure'  => 'file',
+        'owner'   => 'root',
+        'group'   => 'root',
+        'mode'    => '0755',
+        })
+    end
+
+    it do
+      should contain_file('/opt/splunkforwarder/etc/system/local/deploymentclient.conf').with({
+        'ensure'  => 'file',
+        'owner'   => 'root',
+        'group'   => 'root',
+        'mode'    => '0644',
+        })
+    end
+
+    it do
+      should contain_service('splunkforwarder').with({
+        'ensure'     => 'running',
+        'enable'     => 'true',
+      })
+    end
   end
 end
